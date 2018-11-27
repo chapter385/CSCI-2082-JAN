7232a0d4ebdd05009397c38c81a8efb8f28bf3ff
chapter385 <chapter385@gmail.com>
2018-11-26 21:32:32 -0600

    update GUI 11/26

--- CSCI-2082-JAN/src/display/GameUI.java
+++ CSCI-2082-JAN/src/display/GameUI.java
@@ -1,4 +1,5 @@
 package display;
+
 //updated
 import java.awt.BorderLayout;
 import java.awt.Button;
@@ -20,25 +21,25 @@ import data.Driver;
 
 @SuppressWarnings("serial")
 public class GameUI extends JFrame implements ActionListener {
-	
+
 	// Hold the grid of box buttons
 	private BoxButton[][] boxes = new BoxButton[5][5];
-	
+
 	private Button start = new Button("Start Game");
-	
+
 	// Manipulate the sequence of box Object
-	private Driver driver = new Driver();	
-	
+	private Driver driver = new Driver();
+
 	// Hold the sequence of box objects selected by player
 	private BoxQueue inputCollection = new BoxQueue();
-	
+
 	// Hold the sequence of BoxButton objects selected by player
 	ArrayList<BoxButton> inputList = new ArrayList<BoxButton>();
-	
+
 	JLabel levelGUI = new JLabel("Level: " + driver.difficulty.getLevel());
-	
+
 	int levelCount = driver.difficulty.getLevel();
-	
+
 	public GameUI() {
 		super();
 		this.setVisible(true);
@@ -48,30 +49,60 @@ public class GameUI extends JFrame implements ActionListener {
 		this.setLocation(500, 100);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
-						
+
 		JPanel control = new JPanel();
 		control.setLayout(new FlowLayout());
 		start.addActionListener(this);
 		control.add(start);
 		control.add(levelGUI);
 		this.add(control, BorderLayout.NORTH);
-		
+
 		JPanel boxMatrix = new JPanel();
-		boxMatrix.setLayout(new GridLayout(5,5));
-		for(int i=0;i<5;i++)
-			for(int j=0;j<5;j++) {								
-				boxes[i][j] = new BoxButton("",i,j);
+		boxMatrix.setLayout(new GridLayout(5, 5));
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++) {
+				boxes[i][j] = new BoxButton("", i, j);
 				boxes[i][j].addActionListener(this);
 				boxes[i][j].setEnabled(false);
 				boxMatrix.add(boxes[i][j]);
-			}		
+			}
 		this.add(boxMatrix, BorderLayout.CENTER);
-		
+
 	}
-	
+
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		GameUI test = new GameUI();
+	}
+
+	public void displayPattern() {
+		BoxQueue tempQueue;
+
+		tempQueue = driver.generateCollection();
+		ArrayList<BoxButton> tempBoxes = new ArrayList<>();
+
+		for (Box temp : tempQueue.getArray()) {
+			tempBoxes.add(boxes[temp.getX()][temp.getY()]);
+		}
+
+		for (BoxButton button : tempBoxes) {
+			button.setBackground(Color.BLUE);
+
+			Thread.currentThread();
+			try {
+				Thread.sleep(400);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		for (BoxButton button : tempBoxes) {
+			button.setBackground(null);
+		}
+
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
+				boxes[i][j].setEnabled(true);
 	}
 
 	@Override
@@ -80,35 +111,10 @@ public class GameUI extends JFrame implements ActionListener {
 		// When the "Start Game" button is clicked
 		// The program display the sequence of boxes
 		if (arg0.getSource().equals(start)) {
-			BoxQueue tempQueue;
-
-			tempQueue = driver.generateCollection();
-			ArrayList<BoxButton> tempBoxes = new ArrayList<>();
-
-			for (Box temp : tempQueue.getArray()) {
-				tempBoxes.add(boxes[temp.getX()][temp.getY()]);
-			}
-
-			for (BoxButton button : tempBoxes) {
-				button.setBackground(Color.BLUE);
-
-				Thread.currentThread();
-				try {
-					Thread.sleep(400);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-
-			for (BoxButton button : tempBoxes) {
-				button.setBackground(null);
-			}
-
-			for (int i = 0; i < 5; i++)
-				for (int j = 0; j < 5; j++)
-					boxes[i][j].setEnabled(true);
+			displayPattern();
+			start.setEnabled(false);
+			// start.setVisible(false);
 		}
-
 		// Receive the input of player and return the result
 		else if (arg0.getSource() instanceof BoxButton) {
 			BoxButton source = (BoxButton) arg0.getSource();
@@ -128,33 +134,51 @@ public class GameUI extends JFrame implements ActionListener {
 
 			if (inputList.size() == driver.difficulty.getNumberOfBoxes()) {
 				String result;
+				Object[] options = new Object[2];
+
 				try {
 					if (driver.compare(inputCollection)) {
 						result = "Correct Answer";
-						//Increases level
+						// Increases level
 						driver.difficulty.setLevel(++levelCount);
-						//Updates the JLabel to display the current level
-						levelGUI.setText("Level: " +  driver.difficulty.getLevel());
+						// Set options
+						options[0] = "Continue";
+						options[1] = "Quit";
 					} else {
 						result = "Wrong Answer";
-						//Resets game back to level 1
+						// Resets game back to level 1
 						driver.difficulty.setLevel(1);
-						//Updates the JLabel to display the current level
-						levelGUI.setText("Level: " +  driver.difficulty.getLevel());
+						// Set options
+						options[0] = "Retry";
+						options[1] = "Quit";
 					}
-					JOptionPane.showMessageDialog(this, result, "Result", JOptionPane.INFORMATION_MESSAGE);
+					int input = JOptionPane.showOptionDialog(this, result, "Result", JOptionPane.YES_NO_OPTION,
+							JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
-					for (BoxButton element : inputList) {
+					// Updates the JLabel to display the current level
+					levelGUI.setText("Level: " + driver.difficulty.getLevel());
+
+					for (BoxButton element : inputList)
 						element.setBackground(null);
-					}
-					
+
 					inputList.clear();
 					inputCollection.clear();
 
 					for (int i = 0; i < 5; i++)
 						for (int j = 0; j < 5; j++)
 							boxes[i][j].setEnabled(false);
-					
+
+//					JOptionPane.showMessageDialog(this, result, "Result", JOptionPane.INFORMATION_MESSAGE);
+
+					if (input == JOptionPane.YES_OPTION) {
+						if (driver.difficulty.getLevel() == 1) {
+							start.setEnabled(true);
+							// start.setVisible(true);
+						} else
+							displayPattern();
+					} else {
+						this.dispose();
+					}
 				} catch (CloneNotSupportedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -165,5 +189,4 @@ public class GameUI extends JFrame implements ActionListener {
 		}
 
 	}
-
 }
